1895c4a736dfaa72e40b74dfa8d44b4d91cfa3f8
Olena Ageyeva <eageeva@hotmail.com>
2018-06-02 18:01:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let store = { drivers: [], passengers: [], trips: [] };
 let driverId = 0;
 
 class Driver{
-  constructor(name){
-    this.name=name;
-    this.id=driverID+1;
+  constructor(name) {
+    this.name = name;
+    this.id = driverId++;
+    store.drivers.push(this);
   }
-  store.drivers.push(this)
 }
